cf241a94159376bfb0bfe9f87b39eb757876e5f7
Sandro008 <sgonashvili92@gmail.com>
2021-04-29 20:04:54 +0300

    added top-slider and products

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function () {
+    $('.top-slider__inner').slick({
+        arrows: false,
+        dots: true
 
-})
+    })
+});
+
+$(function () {
+    var mixer = mixitup(' .products-content ');
+});
