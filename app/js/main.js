8ae0a2ce728ec4d0e087e8128da3b3a809ebc053
Sandro008 <sgonashvili92@gmail.com>
2021-06-09 16:25:43 +0300

    added product-list

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,69 @@
 $(function () {
-    $('.menu-nav__menu').on('click', function () {
-        $('.menubar').removeClass('menubar--close');
+    $('.catalog-content__btn').on('click', function () {
+        $('.catalog-content__btn').removeClass('catalog-content__btn--active');
+        $(this).addClass('catalog-content__btn--active');
     });
-     $('.menubar__close').on('click', function () {
-         $('.menubar').addClass('menubar--close');
+
+    $('.button-list').on('click', function(){
+        $('.catalog-content__menu').addClass('catalog-content__menu--list');
+    });
+     $('.button-grid').on('click', function () {
+         $('.catalog-content__menu').removeClass('catalog-content__menu--list');
      });
+
+
+});
+
+
+$(function () {
+    $('.catalog__input').ionRangeSlider({
+        type: "double",
+        prefix: "$",
+        onStart: function (data) {
+            $('.catalog__min').text(data.from),
+                $('.catalog__max').text(data.to)
+        },
+        onChange: function (data) {
+            $('.catalog__min').text(data.from);
+            $('.catalog__max').text(data.to);
+        },
+    });
 });
 
 $(function () {
-    $('.catalg__input').ionRangeSlider();
+
+    $('.select-style').styler()
+})
+
+$(function () {
+
+    $(".catalog__star").rateYo({
+        starWidth: "12px",
+        normalFill: "#d6d6d6",
+        ratedFill: "#ffcc00",
+        readOnly: true
+    });
+
+});
+$(function () {
+
+    $(".product-card__star").rateYo({
+        starWidth: "20px",
+        normalFill: "#d6d6d6",
+        ratedFill: "#ffcc00",
+        readOnly: true
+    });
+
 });
 
-
+$(function () {
+    $('.menu-nav__menu').on('click', function () {
+        $('.menubar').removeClass('menubar--close');
+    });
+    $('.menubar__close').on('click', function () {
+        $('.menubar').addClass('menubar--close');
+    });
+});
 
 $(function () {
     $('.top-slider__inner').slick({
@@ -20,7 +72,7 @@ $(function () {
         autoplay: true,
         autoplaySpeed: 2000,
 
-    })
+    });
 });
 
 $(function () {
@@ -32,11 +84,11 @@ $(function () {
         infinite: true,
         slidesToShow: 5,
         slidesToScroll: 1,
-        touchMove:true
+        touchMove: true
 
-    })
+    });
 
-})
+});
 
 
 $(function () {
